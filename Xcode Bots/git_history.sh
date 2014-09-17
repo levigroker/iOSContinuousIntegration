5ddc43fc9ff4f6b823e3ee1d145d9fdc05b8fca3
Levi Brown <levigroker@gmail.com>
2014-09-16 20:35:05 -0600

    Adding git_history.sh

---
+++ Xcode Bots/git_history.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Pulls git commit history between two revision hashes and returns a formatted result.
+#
+# Usage is as simple as:
+#
+# % cd <your repo>
+# % git_history.sh 663bc9427b609554c57663196b6a36651df6f3c1
+#
+# Which will fetch all history from the commit with the given hash to HEAD.
+#
+# Supply a second hash to specify a range:
+#
+# % cd <your repo>
+# % git_history.sh 663bc9427b609554c57663196b6a36651df6f3c1 eebf0e48a10dfc43b4d579734c0199941171376f
+#
+# The output format defaults to "%ai %an: %B" but can be overridden by exporting
+# `GIT_LOG_FORMAT` with the desired format string (see 'git help log' for format details).
+#
+# Levi Brown
+# mailto:levigroker@gmail.com
+# September 16, 2014
+# https://github.com/levigroker/iOSContinuousIntegration
+##
+
+function usage()
+{
+	[[ "$@" = "" ]] || echo "$@" >&2
+	echo "Usage:" >&2
+	echo "$0 <start hash> [<end hash>]" >&2
+    exit 1
+}
+
+function fail()
+{
+    echo "Failed: $@" >&2
+    exit 1
+}
+
+DEBUG=${DEBUG:-0}
+export DEBUG
+
+set -eu
+[ $DEBUG -ne 0 ] && set -x
+
+# Release note formatting (See 'git help log' for format details)
+export GIT_LOG_FORMAT=${GIT_LOG_FORMAT:-"%ai %an: %B"}
+
+# Fully qualified binaries (_B suffix to prevent collisions)
+GIT_B="/usr/bin/git"
+
+START_REV=${1:-""}
+END_REV=${2:-""}
+
+if [ "$START_REV" = "--help" -o "$START_REV" = "-h" ]; then
+	usage
+fi
+
+if [ "$START_REV" = "" ]; then
+	usage "Empty starting commit hash specified."
+fi
+
+if [ "$END_REV" = "" ]; then
+	END_REV=$($GIT_B rev-parse --verify HEAD)
+fi
+
+RELEASE_NOTES=$($GIT_B log --notes --reverse --pretty="$GIT_LOG_FORMAT" $START_REV..$END_REV)
+[ "$RELEASE_NOTES" != "" ] && RELEASE_NOTES="Commit history from hash \"$START_REV\" to hash \"$END_REV\" (inclusive)"$'\n'"---"$'\n'"$RELEASE_NOTES"
+echo "$RELEASE_NOTES"
